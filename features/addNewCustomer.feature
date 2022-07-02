29eb33614d15942e36f9b3a4b79cb6e8d2001d50
SirunH <97840858+SirunH@users.noreply.github.com>
2022-07-02 17:54:33 +0400

    Add files via upload

--- features/addNewCustomer.feature
+++ features/addNewCustomer.feature
@@ -2,7 +2,8 @@ Feature: New customer creation
 
   Scenario: As a Bank Manager I can create new customer
 
-    Given I am in login page and want to add new customer
-    When I fill need information in addCust page
-    When I search the new created customer by post code in customers list, the customer is filtered from the list and after I click in delete button
-    Then I search the deleted customer again and there is no any information about that customer
+    Given I am in login page 
+    When I fill fields in addCust page
+    Then I should accept alert text
+    Then I should click in delete button
+    Then I should verify there is no any information about new customer
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,5 +1,4 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
-//const { default: pause } = require('webdriverio/build/commands/browser/pause');
 const LoginPage = require('../pageobjects/login.page');
 const registrationPage = require('../pageobjects/registration.page');
 const faker = require("@faker-js/faker").faker;
@@ -11,17 +10,20 @@ const postCode = faker.address.zipCode();
 
 
 
-Given("I am in login page and want to add new customer", async() => {
+Given("I am in login page", async() => {
     await LoginPage.open();
     await LoginPage.goToLoginPage();
     await expect(browser).toHaveUrlContaining(helper.managerPath);
     await expect(browser).toHaveTitle(helper.pageTitle);
 });
 
-When("I fill need information in addCust page", async() => {
+When("I fill fields in addCust page", async() => {
     await registrationPage.addCustomer();
     await expect(browser).toHaveUrlContaining(helper.addCustPath);
     await registrationPage.proceedRegistration(firstName, lastName, postCode);
+
+});
+Then("I should accept alert text", async() => {
     await browser.waitUntil(async() => {
         return browser.isAlertOpen();
     });
@@ -30,7 +32,7 @@ When("I fill need information in addCust page", async() => {
     await browser.acceptAlert();
 });
 
-When("I search the new created customer by post code in customers list, the customer is filtered from the list and after I click in delete button", async() => {
+Then("I should click in delete button", async() => {
     await registrationPage.customersbutton();
     await expect(browser).toHaveUrlContaining(helper.allcustomersListPath);
     await registrationPage.getSearchField(postCode);
@@ -38,7 +40,8 @@ When("I search the new created customer by post code in customers list, the cust
     await registrationPage.getDeleteButton();
     await expect(customerInfo).not.toBeExisting();
 });
-Then("I search the deleted customer again and there is no any information about that customer", async() => {
+
+Then("I should verify there is no any information about new customer", async() => {
     await registrationPage.getSearchFieldSecondTime(postCode);
     let customerInfo = await registrationPage.customerinfo;
     await expect(customerInfo).not.toBeExisting();
